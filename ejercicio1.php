a774b0b7c186e63cc8c94542b3c2c862fe505586
Moises Carretero <moises.carretero@unidadeditorial.es>
2023-07-01 11:29:29 +0200

    Borro cosas

--- ejercicio1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $titulo = $_GET['titulo'];
-
-    require_once "template.html";
-
-    if ($resuelto){
-        echo "Se ha hecho bien";
-    }
--- funciones.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    declare(strict_types=1);
-    require_once "./lib/utils.php";
-
-    echo suma(num2:4, num1:6, num3: 10);
-    //echo "La suma vale $result";
-    saludo("Laura");
